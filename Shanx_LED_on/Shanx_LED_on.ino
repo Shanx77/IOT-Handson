3a899596168c113a44f5f32828485540f16079cc
Shanx <shashankdevashettyl@gmail.com>
2024-03-22 12:22:48 +0530

    shanx first commit

---
+++ Shanx_LED_on/Shanx_LED_on.ino
@@ -0,0 +1,11 @@
+const int LED = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  // put your main code here, to run repeatedly:
+
+}
